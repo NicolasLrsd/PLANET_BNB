4aacacc06643806a064215d137e1319865e9b530
Nicolas <nicolas.lisardipro@gmail.com>
2024-05-27 17:42:57 +0200

    CRUD planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,10 +1,27 @@
 class PlanetsController < ApplicationController
+
   def index
+    @planets = Planet.all
+  end
+
+  def show
+    @planet = Planet.find(param[:id])
+
   end
 
   def new
+    @planet = Planet.new
   end
 
   def create
+    @planet = Planet.new(planets_params)
+    @planet.save
+    redirect_to planet_path(@planets)
+  end
+
+  private
+
+  def planets_params
+    params.require(:planet).permit(:name, :detail, :price)
   end
 end
