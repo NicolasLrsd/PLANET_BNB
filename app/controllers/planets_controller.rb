ca82d149af9d63fbd5c64cc20719f969d8e3d9dd
collet remi <remi.collet.perso@hotmail.com>
2024-05-28 15:23:52 +0200

    add login button and assign user to a planet when create

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -14,6 +14,7 @@ class PlanetsController < ApplicationController
 
   def create
     @planet = Planet.new(planet_params)
+    @planet.user = current_user
     @planet.save
     redirect_to planet_path(@planet)
   end
